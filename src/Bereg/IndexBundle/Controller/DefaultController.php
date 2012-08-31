d4a8c43cad8066ebb7000dc61d2ef2d180b70fdf
PiVolan <pivolan@gmail.com>
2012-09-01 03:39:35 +0400

    Добавление в контроллер связей с аннотацией для security

--- src/Bereg/IndexBundle/Controller/DefaultController.php
+++ src/Bereg/IndexBundle/Controller/DefaultController.php
@@ -3,10 +3,12 @@
 namespace Bereg\IndexBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
 
 // these import the "@Route" and "@Template" annotations
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use JMS\SecurityExtraBundle\Annotation\Secure;
 
 class DefaultController extends Controller
 {
